a123bf8fd98211d95acfc0a6ff01864c3fdb09b7
Nathan Willson <willson.b.nathan@gmail.com>
2018-11-28 10:21:33 -0800

    A bit of metadata-ness

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,8 +25,8 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: 'description', content: 'Time zone difference tool for finding time to chat' },
+            { name: 'keywords', content: 'time zone, time difference, time calculator, chat, time zone chat, when to chat, whentochat, time overlap' },
           ]}
         >
           <html lang="en" />
