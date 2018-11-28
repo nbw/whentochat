88ff744a77f87ee2ab7232b7d2091ad37dd50ebd
Nathan Willson <willson.b.nathan@gmail.com>
2018-11-27 21:33:42 -0800

    Add new favicon; remove extra images; clean up config

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,17 +18,10 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ],
 }
