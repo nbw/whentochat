bcf3365e8233a313cc3766f3d76773f19073349f
Nathan Willson <willson.b.nathan@gmail.com>
2018-12-04 08:44:32 -0800

    Metadata
    * Twitter card
    * Keywords
    * Description

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,8 @@ import Footer from './footer'
 import './layout.css'
 import './fontawesomestyles.css'
 
+import { withPrefix } from 'gatsby'
+
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
@@ -23,19 +25,22 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Time zone difference tool for finding time to chat' },
-            { name: 'keywords', content: 'time zone, time difference, time calculator, chat, time zone chat, when to chat, whentochat, time overlap' },
-          ]}
-        >
-          <html lang="en" />
-					<link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet" />
-					<meta charset="utf-8"/>
-					<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-					<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        </Helmet>
+      <Helmet
+        title={data.site.siteMetadata.title}
+      >
+        <html lang="en" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet" />
+        <meta charset="utf-8"/>
+        <meta name="description" content="Time zone difference tool for finding time to chat" />
+        <meta name="keywords" content="time zone,time difference,time calculator,chat,time zone chat,when to chat,whentochat,time overlap" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@nathanwillson" />
+        <meta name="twitter:title" content="When To Chat" />
+        <meta name="twitter:description" content="Reconcile time zone differences, so that chatting is easy." />
+        <meta name="twitter:image" content={withPrefix('/banner.png')} />
+      </Helmet>
       <div className='mainContainer' >
         <Header siteTitle={data.site.siteMetadata.title} siteSubtitle={data.site.siteMetadata.subtitle} />
         <div
