df4edc8ec079f5eec4310a78363838f6de51d1ab
Nathan Willson <willson.b.nathan@gmail.com>
2019-10-18 23:38:02 +0900

    Fix emoji warning

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -8,12 +8,12 @@ const FaqPage = () => (
     <h1>Frequently Asked Questions</h1>
     <h2>1. Where's the search button?</h2>
     <p>
-      There isn't one! Once you've selected two locations from the location input dropdowns, a search is automatically triggered. If you're not seeing dropdown results, then refer to #2. Sounds like a browser issue. 🙃
+      There isn't one! Once you've selected two locations from the location input dropdowns, a search is automatically triggered. If you're not seeing dropdown results, then refer to #2. Sounds like a browser issue. <span role="img" aria-label="oops">🙃</span>
     </p>
     <h2>2. Search not working?</h2>
-		<p>
+    <p>
       Sometimes the dropdown input fields don't work. This is typically due to a browser plugin that's mucking things up. <b>Try disabling your browser plugins, and/or trying a differnt browser entirely.</b> If you're having issues, please send me an email (with your setup). I'd appreciate it a lot.
-		</p>
+    </p>
     <p>This is what you should be expecting to happen:</p>
     <img
       style={{
